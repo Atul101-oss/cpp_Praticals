dc4b046fa4bedb5e03bd38b86a69e77d9bc51675
Atul_Arya <atularya010@gmail.com>
2023-10-25 07:31:27 +0530

    25 oct 7:31

--- P1_CSC-22-11_DS.cpp
+++ P1_CSC-22-11_DS.cpp
@@ -7,33 +7,43 @@ iii.Remove an element from the beginning of the doubly linked listiv.
 vi.Search for an element x in the singly linked list and return its pointe
 */
 
-#include <iostream>
-#include <vector>
-using namespace std;
+// #include <iostream>
+// using namespace std;
 
-class node {
-    public:
-    int data;
-    node *next;
-
-    node(int d) {
+#include <iostream>  
+using namespace std;  
+class Node {  
+   public:  
+    int data;    
+    Node *next;
+    Node (int d=5){
         data = d;
-        next = NULL;
-    }
-};
-void test() {
-    vector <int> V;
-    cout << V.size()<<endl;
-    for (int i = 0; i < 5; i++)
-    {
-        V.push_back(i);
-    };
-    cout << V.size();
-};
 
-int main(){
-    // node *head;
-    test();
-  
+    }
+};  
+
+void InsertAtHead(Node* &head, int value){
+    Node *newNode = new Node(value);
+    newNode->next = head;
+    head = newNode;
+    cout <<head->data<<"Insertion complete";
 }
 
+void display(Node* head){
+    Node *current = head;
+    while (current != NULL){
+        cout << current <<" -> ";
+        current = current->next;
+        
+    }
+    cout <<"NULL";
+    cout << "Cout complete";
+}
+
+int main() {
+    Node *head = NULL;
+
+    InsertAtHead(head,16);
+    display(head);
+    return 0;
+}  
