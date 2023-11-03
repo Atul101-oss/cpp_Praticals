0263a6b4bef692026e70ec5e06e018377cb954bd
Atul_Arya <76675402+Atul101-oss@users.noreply.github.com>
2023-11-03 14:15:56 +0530

    Update

---
+++ pt2.cpp
@@ -0,0 +1,6 @@
+Write a program to implement doubly linked list as an ADT that supports the following op-
+erations:
+i. Insert an element x at the beginning of the doubly linked list
+ii. Insert an element x at the end of the doubly linked list
+iii. Remove an element from the beginning of the doubly linked list
+iv. Remove an element from the end of the doubly linked list
