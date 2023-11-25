f5d5b15568db579c97e23bbcdca8088b13b2f190
Atul_Arya <atularya010@gmail.com>
2023-11-25 12:39:51 +0530

    OS_FileFolder added

---
+++ PracticalFileOS/P1_CSC-22-11_OS.cpp
@@ -0,0 +1,16 @@
+/*
+1. Execute various LINUX commands for:
+i. Information Maintenance: wc, clear, cal, who, date, pwd
+ii. File Management: cat, cp, rm, mv, cmp, comm, diff, find, grep, awk
+iii. Directory Management: cd, mkdir, rmdir, ls
+2. Execute various LINUX commands for:
+*/
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    /* code */
+    cout << "$file" <<endl;
+    return 0;
+}
