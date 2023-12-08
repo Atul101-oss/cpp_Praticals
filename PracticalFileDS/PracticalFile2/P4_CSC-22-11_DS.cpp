0baeb954461eb9035e7bdf3ef5dd052d6d8459db
Atul_Arya <76675402+Atul101-oss@users.noreply.github.com>
2023-12-08 16:31:33 +0530

    Update P4_CSC-22-11_DS.cpp
    add Manu driven

--- PracticalFileDS/PracticalFile2/P4_CSC-22-11_DS.cpp
+++ PracticalFileDS/PracticalFile2/P4_CSC-22-11_DS.cpp
@@ -3,8 +3,8 @@
 #include <sstream>
 using namespace std;
 
-int performPostFix(char operator1, int operand1, int operand2){
-    switch(operator1){
+int performPostFix(char operator1, int operand1, int operand2) {
+    switch (operator1) {
         case '+':
             return operand1 + operand2;
         case '-':
@@ -13,49 +13,80 @@ int performPostFix(char operator1, int operand1, int operand2){
             return operand1 * operand2;
         case '/':
             if (operand2 != 0)
-                return operand1/operand2;
+                return operand1 / operand2;
             else
                 throw runtime_error("Division by 0");
-        
-        default :
-            throw runtime_error("invalid operator");
+        default:
+            throw runtime_error("Invalid operator");
     }
 }
 
-int evaluatePostFix(string expration){
+int evaluatePostFix(string expression) {
     stack<int> stackPF;
-    
-    istringstream iss(expration);
+
+    istringstream iss(expression);
     string term;
-    
-    while(iss >> term){
+
+    while (iss >> term) {
         if (isdigit(term[0]) || (term[0] == '-' && isdigit(term[1]))) {
             stackPF.push(stoi(term));
-        }else{
+        } else {
             int operand1 = stackPF.top();
             stackPF.pop();
             int operand2 = stackPF.top();
             stackPF.pop();
-            int result = performPostFix(term[0],operand1,operand2);
+            int result = performPostFix(term[0], operand1, operand2);
             stackPF.push(result);
         }
     }
     if (stackPF.size() == 1)
         return stackPF.top();
     else
-        throw runtime_error("Invalid expration");
+        throw runtime_error("Invalid expression");
 }
 
-int main (int argc, char* argv[]){
-    string expration;
-    expration = "3 4 + 5 *";
-    cout << "Enter a PostFix : ";
-    cin >> expration;
-    
-    if (expration.size() <= 2){
-        expration = "3 7 + 5 *";
-    }
-    cout << "PostFix = " <<expration <<endl;
-    cout << "-------------------------------------\n";
-    cout << expration << " = " <<evaluatePostFix(expration) <<endl;
+int main() {
+    string expression;
+
+    int choice;
+    do {
+        cout << "\nMenu:\n";
+        cout << "1. Enter a PostFix expression\n";
+        cout << "2. Evaluate the last entered expression\n";
+        cout << "3. Exit\n";
+
+        cout << "Enter your choice: ";
+        cin >> choice;
+
+        switch (choice) {
+            case 1:
+                cout << "Enter a PostFix expression: ";
+                cin.ignore(); // Clear the newline character from the buffer
+                getline(cin, expression);
+                break;
+
+            case 2:
+                if (expression.empty()) {
+                    cout << "No expression entered yet. Please enter a PostFix expression first.\n";
+                } else {
+                    cout << "PostFix expression: " << expression << endl;
+                    try {
+                        cout << "Result: " << evaluatePostFix(expression) << endl;
+                    } catch (const exception& e) {
+                        cerr << "Error: " << e.what() << endl;
+                    }
+                }
+                break;
+
+            case 3:
+                cout << "Exiting program.\n";
+                break;
+
+            default:
+                cout << "Invalid choice. Try again.\n";
+        }
+
+    } while (choice != 3);
+
+    return 0;
 }
