442c44f48c24e19e7a4c93b1617b24e4e0bf8810
Atul_Arya <76675402+Atul101-oss@users.noreply.github.com>
2023-12-08 16:22:45 +0530

    Update P1_CSC-22-11_DS.cpp
    Manu driven

--- PracticalFileDS/PracticalFile1/P1_CSC-22-11_DS.cpp
+++ PracticalFileDS/PracticalFile1/P1_CSC-22-11_DS.cpp
@@ -120,22 +120,71 @@ public:
 
 int main() {
     SinglyLinkedList linked_list;
-    linked_list.insert_at_beginning(3);
-    linked_list.insert_at_beginning(2);
-    linked_list.insert_at_beginning(1);
-    linked_list.insert_at_position(4, 2);
-    linked_list.display();
 
-    linked_list.remove_from_beginning();
-    linked_list.remove_from_position(2);
-    linked_list.display();
+    int choice, data, position;
 
-    Node* search_result = linked_list.search(2);
-    if (search_result) {
-        cout << "Found: " << search_result->data << endl;
-    } else {
-        cout << "Not Found" << endl;
-    }
+    do {
+        cout << "\nMenu:\n";
+        cout << "1. Insert at beginning\n";
+        cout << "2. Insert at position\n";
+        cout << "3. Remove from beginning\n";
+        cout << "4. Remove from position\n";
+        cout << "5. Search\n";
+        cout << "6. Display\n";
+        cout << "7. Exit\n";
+
+        cout << "Enter your choice: ";
+        cin >> choice;
+
+        switch (choice) {
+            case 1:
+                cout << "Enter data to insert at the beginning: ";
+                cin >> data;
+                linked_list.insert_at_beginning(data);
+                break;
+
+            case 2:
+                cout << "Enter data to insert: ";
+                cin >> data;
+                cout << "Enter position to insert at: ";
+                cin >> position;
+                linked_list.insert_at_position(data, position);
+                break;
+
+            case 3:
+                linked_list.remove_from_beginning();
+                break;
+
+            case 4:
+                cout << "Enter position to remove: ";
+                cin >> position;
+                linked_list.remove_from_position(position);
+                break;
+
+            case 5:
+                cout << "Enter data to search: ";
+                cin >> data;
+                Node* search_result = linked_list.search(data);
+                if (search_result) {
+                    cout << "Found: " << search_result->data << endl;
+                } else {
+                    cout << "Not Found" << endl;
+                }
+                break;
+
+            case 6:
+                linked_list.display();
+                break;
+
+            case 7:
+                cout << "Exiting program.\n";
+                break;
+
+            default:
+                cout << "Invalid choice. Try again.\n";
+        }
+
+    } while (choice != 7);
 
     return 0;
 }
